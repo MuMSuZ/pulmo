d0ae01b8f2d48f2316e8712e509208b9bc307c67
MuMSuZ <mumsuz@outlook.com>
2024-06-17 00:43:43 +0300

    update

--- koah/koah.js
+++ koah/koah.js
@@ -52,7 +52,7 @@ var acc = document.getElementsByClassName("accordion");
                                 if (line.includes('**')) {
                                     const parts = line.split('**').map((part, index) => {
                                         if (index % 2 === 1) { // Tek indeksler kalın kısımlar olacak
-                                            return `<span class="strong">${part.trim()}</span>`;
+                                            return `<bold>${part.trim()}</bold>`;
                                         }
                                         return part;
                                     });
