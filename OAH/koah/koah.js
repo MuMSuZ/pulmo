745c82592efcb27ce0b792fcc35a0e5b7cfe4f83
mumsuz <mumsuz@outlook.com>
2024-06-17 00:52:09 +0300

    update

---
+++ OAH/koah/koah.js
@@ -0,0 +1,38 @@
+var acc = document.getElementsByClassName("accordion");
+        var i;
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                    panel.style.display = "none";
+                } else {
+                    panel.style.display = "block";
+                }
+                if (panel.id === 'tanım') {
+                    loadContent('tanım.txt', panel); // İçeriği yüklemek için panel id'sini kontrol edin
+                }
+                if (panel.id === 'yuk') {
+                    loadContent('yuk.txt', panel);
+                }
+                var arrow = this.querySelector(".arrow");
+                if (arrow.style.transform === "rotate(90deg)") {
+                    arrow.style.transform = "rotate(0deg)";
+                } else {
+                    arrow.style.transform = "rotate(90deg)";
+                }
+            
+            });
+        }
+
+        function loadContent(url, element) {
+            fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    element.innerHTML = data;
+                })
+                .catch(error => {
+                    console.error('Error loading content:', error);
+                    element.innerHTML = 'İçerik yüklenemedi.';
+                });
+        }
